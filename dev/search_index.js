1f34406901396bcd0e4fda27cc541d7a2cd5fada
Documenter.jl <documenter@juliadocs.github.io>
2023-02-07 10:56:50 +0000

    build based on 3f0c92b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TourOfJuliaExamTemplate","category":"page"},{"location":"#TourOfJuliaExamTemplate","page":"Home","title":"TourOfJuliaExamTemplate","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TourOfJuliaExamTemplate.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TourOfJuliaExamTemplate]","category":"page"}]
+}
